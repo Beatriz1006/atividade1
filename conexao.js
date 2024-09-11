e79b86b976b188bc749def27b9c4f23e1d1b5fa0
Beatriz1006 <biasb1006@gmail.com>
2024-09-11 14:10:37 -0300

    Create conexao.js

---
+++ conexao.js
@@ -0,0 +1,12 @@
+ar mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "seuhost",
+  user: "seuuser",
+  password: "seupass"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
